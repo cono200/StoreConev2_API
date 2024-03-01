7dbbb389fa0621f5fe5966bdbd4aae7bab5eb0d2
PC <danyel.16@gmail.com>
2024-02-29 21:07:37 -0600

    a

---
+++ StoreCone.Api/Models/MermasModel.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace StoreCone.Api.Models
+{
+    public class MermasModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("Codigo")]
+
+        public string Codigo { get; set; } = string.Empty;
+        [BsonElement("Tipo_de_merma")]
+
+        public string Tipo_de_merma { get; set; } =string.Empty;
+        [BsonElement("Fecha_ingreso")]
+
+        public DateTime Fecha_ingreso { get; set; } 
+        [BsonElement("Nombre_producto")]
+
+        public string Nombre_producto { get; set; } = string.Empty;
+    }
+}
--- StoreCone.Api/Program.cs
+++ StoreCone.Api/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<ProveedorServices>();
+builder.Services.AddSingleton<MermaServices>();
 
 var app = builder.Build();
 
